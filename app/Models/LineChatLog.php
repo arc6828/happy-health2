5dedfca6e80dee05a2ef5a731293d0492704cbcb
Chavalit Koweerawong <chavalit.kow@gmail.com>
2025-05-29 21:00:42 +0700

    Update LineChatLog.php

--- app/Models/LineChatLog.php
+++ app/Models/LineChatLog.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class LineChatLog extends Model
 {
     //
+    protected $fillable = [
+        'line_user_id',
+        'prompt',
+        'reply',
+    ];
+
 }
